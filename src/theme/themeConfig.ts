1e86212e00b8f139f10d93630d0f36cc43ce447a
Matheus Matos <matheus@matheusmatos.com>
2023-12-18 13:48:33 -0300

    build: configure antd initial theme

---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,12 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    colorSuccess: '#62bb35',
+    borderRadius: 0,
+    colorPrimary: '#1d7dff',
+    colorInfo: '#1d7dff',
+  },
+};
+
+export default theme;
